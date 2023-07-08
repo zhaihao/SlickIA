9a7904fc6f68d60a45c859d7c49eab984997c71a
zhaihao <zhaihao@ooon.me>
2023-07-08 09:28:18 +0800

    update: h2 2.1.214 -> 2.2.220 (#72)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies extends AutoPlugin {
     lazy val HIKARI          = "com.zaxxer"              % "HikariCP"                      % "5.0.1"
     lazy val POSTGRES        = "org.postgresql"          % "postgresql"                    % "42.6.0"
     lazy val MYSQL           = "mysql"                   % "mysql-connector-java"          % "8.0.33"
-    lazy val H2              = "com.h2database"          % "h2"                            % "2.1.214" % Test
+    lazy val H2              = "com.h2database"          % "h2"                            % "2.2.220" % Test
 
     lazy val SLICK = Seq(
       "com.typesafe.slick"  %% "slick"              % "3.4.1",
