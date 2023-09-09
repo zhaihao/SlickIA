52bd69a2d82b2ca2584366a292fc51ea298627a5
zhaihao <zhaihao@ooon.me>
2023-09-09 08:58:28 +0800

    update: log4j-over-slf4j 2.0.7 -> 2.0.9 (#90)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies extends AutoPlugin {
     )
 
     lazy val LOG = Seq(
-      "org.slf4j"                   % "log4j-over-slf4j" % "2.0.7",
+      "org.slf4j"                   % "log4j-over-slf4j" % "2.0.9",
       "com.typesafe.scala-logging" %% "scala-logging"    % "3.9.5",
       "ch.qos.logback"              % "logback-classic"  % "1.4.11"
     )
