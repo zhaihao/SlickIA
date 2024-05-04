54fb05d9e07dea108a51e24e27b5c60197d991a9
zhaihao <zhaihao@ooon.me>
2024-05-04 09:02:01 +0800

    update: mysql-connector-j 8.3.0 -> 8.4.0 (#137)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies extends AutoPlugin {
     lazy val CLICKHOUSE      = "com.github.housepower"   % "clickhouse-native-jdbc-shaded" % "2.6.5"
     lazy val HIKARI          = "com.zaxxer"              % "HikariCP"                      % "5.0.1"
     lazy val POSTGRES        = "org.postgresql"          % "postgresql"                    % "42.7.3"
-    lazy val MYSQL           = "com.mysql"                   % "mysql-connector-j"          % "8.3.0"
+    lazy val MYSQL           = "com.mysql"                   % "mysql-connector-j"          % "8.4.0"
     lazy val H2              = "com.h2database"          % "h2"                            % "2.2.224" % Test
 
     lazy val SLICK = Seq(
